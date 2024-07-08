14cc1bb2f89c1f434e42d5acfad65514b59518f4
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-07-07 17:55:45 -0700

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template katrina kadyawa
+      React homework template katrina kadyawa na
     </div>
   );
 };
